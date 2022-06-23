7f16d6e107228fe7b5943c8d510bf605302b510a
anup <anuppal602@gmail.com>
2022-06-23 10:50:18 +0530

    header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,16 +1,72 @@
-import React from 'react'
-import './header.css'
+import {
+  faBed,
+  faCalendarDays,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./header.css";
 const Header = () => {
   return (
     <div className="header">
+      <div className="headerContainer">
         <div className="headerList">
-        <div className="headerListItem">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discount? It's Genius</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels unlock instant savings of 10% or more
+          with a free booking account.
+        </p>
+        <button className="headerBtn">Sign in /Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
             
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <span className="headerSearchText">2 adults and children 1 room</span>
+          </div>
         </div>
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
+
+
+
+
